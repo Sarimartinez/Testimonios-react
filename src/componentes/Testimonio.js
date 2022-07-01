aad85eb39dd6b6fa1fe136c0a88a48d59ffb1fde
unknown <saramartinez060@gmail.com>
2022-07-01 20:21:39 -0300

    cambios

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -5,7 +5,7 @@ function Testimonio() {
         <div className='contenedor-testimonio'>
             <img
                className='imagen-testimonio'
-               src={require('../imagenes/testimonio-emma.png/')}
+               src={require('../imagenes/testimonio-emma.png')}
                alt='Foto de Emma'/>
             <div className='contenedor-texto-testimonio'>
                 <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
